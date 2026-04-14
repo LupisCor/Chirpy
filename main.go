8cbeb91cd112a2a20fc9be08c92080a4ba0f5fc9
Dan <doliveri575@gmail.com>
2026-04-14 17:25:23 -0400

    add handler to main.go to index.html and logo.png

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 
 func main() {
 	const port = "8080"
+	const rootfilepath = "."
 	mux := http.NewServeMux() //Create ServeMux
+	mux.Handle("/", http.FileServer(http.Dir(rootfilepath)))
+	mux.Handle("/assets/logo.png", http.FileServer(http.Dir(rootfilepath)))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", rootfilepath, port)
 	log.Fatal(srv.ListenAndServe())
 
 }
