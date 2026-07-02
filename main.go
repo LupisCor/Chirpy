7be585d373c1efb13c7ec3e23c9d350425485ae6
LupisCor <doliveri575@gmail.com>
2026-07-01 20:22:36 -0400

    implement create users endpoint

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
+	platform       string
 }
 
 func main() {
@@ -24,6 +25,14 @@ func main() {
 
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB-URL must be set in environment variables")
+	}
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set in environment variables")
+	}
+
 	dbconn, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatalf("Error opening database: %s", err)
@@ -33,6 +42,7 @@ func main() {
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             dbQueries,
+		platform:       platform,
 	}
 
 	mux := http.NewServeMux()
@@ -41,6 +51,7 @@ func main() {
 	// HandleFunc format: mux.HandleFunc("/path", handlerFunction)
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)             // Handle readiness check
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp) // Handle chirp validation
+	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)      // Handle user creation
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics) // Handle metrics endpoint
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)    // Handle reset endpoint
